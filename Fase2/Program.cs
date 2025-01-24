9d0fa3d291e84ef4aa5ba7dc61a2f7a7fb9f890a
bia104170 <a104170@alunos.uminho.pt>
2025-01-24 13:24:27 +0000

    Tarte.razor com backend feito

--- Fase2/Program.cs
+++ Fase2/Program.cs
@@ -1,9 +1,25 @@
-using Fase2.Components;
+using Fase2.Features;
 using ui;
 using business;
 
-/*
+
 var builder = WebApplication.CreateBuilder(args);
+
+//É singleton porque deve ser unico para toda a aplicação de modo a evitar inconsistência de dados
+// Registra o GestorProdutos como um serviço Singleton
+//Substituir pela bd se possível
+builder.Services.AddSingleton<IGestorProdutos, GestorProdutos>(sp =>
+{
+    var gestor = new GestorProdutos();
+    gestor.AddProduto("Tarte de Maçã", 8.00f, "maca", "tarte_maca.png", "Uma deliciosa tarte de maçã com um toque de canela e crocância.");
+    gestor.AddProduto("Tarte Rosas", 7.00f, "rosas", "tarte_rosa.png", "Imagine fatias finas de fruta moldadas em rosas.");
+    gestor.AddProduto("Tarte de Mirtilo", 8.50f, "mirtilo", "tarte_mirtilo.png", "Cada fatia contém a combinação perfeita de doçura e acidez. O ideal para completar qualquer refeição.");
+    gestor.AddProduto("Tarte de Framboesa", 8.00f, "framboesa", "tarte_framboesa.png", "Esta é mais uma tarte onde cada fatia traz uma explosão deliciosa de sabores. É ainda melhor quando acompanhada com uma bola de gelado.");
+    gestor.AddProduto("Tarte de Chocolate", 9.00f, "chocolate", "tarte_chocolate.png", "A tarte de chocolate é sempre um prazer irresistível. O suave e cremoso creme de chocolate enriquecido com uma pitada de flor de sal é a combinação perfeita para ficar rendido a esta tarte. Os frutos secos completam estes sabores, ao trazerem toques de crocância.");
+    gestor.AddProduto("Tarte Mandala", 8.00f, "mandala", "tarte_mandala.png", "Uma simples e saborosa tarte que lembra a primavera. A perfeita combinação de sabores no recheio e o bonito desenho no topo fazem desta tarte única e especial");
+
+    return gestor;
+});
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -27,9 +43,9 @@ app.UseAntiforgery();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
-app.Run();*/
+app.Run();
 
-
+/*
 namespace CantinhoDasEncomendas {
 
     public class CantinhoDasEncomendas {
@@ -47,4 +63,4 @@ namespace CantinhoDasEncomendas {
 
     }
 
-}
+} */
